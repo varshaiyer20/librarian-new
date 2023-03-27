0ee252f3479302fe4382ceeff17edb0632eed226
Varsha Iyer <varshaiyer20@gmail.com>
2023-03-27 09:25:08 +0530

    Bottomnavbar,Topnavbar,all json files,Books,Authors,IssuedBooks,Members,Staff

---
+++ src/components/BottomNavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-bottom-navigation app>
+      <v-btn value="recent">
+        <span>Recent</span>
+  
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+  
+      <v-btn value="favorites">
+        <span>Favorites</span>
+  
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+  
+      <v-btn value="nearby">
+        <span>Nearby</span>
+  
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
